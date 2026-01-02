c6956fa7ad9b7fbdcf4faff27b4008123467722b
Math Peeters <pmathieu.peeters@gmail.com>
2026-01-02 15:29:22 +0100

    ajout du gardien engourdi

---
+++ Les Chroniques de DotNetia/Models/Gardien Engourdi.cs
@@ -0,0 +1,32 @@
+﻿using Les_Chroniques_de_DotNetia.Models;
+
+namespace Les_Chroniques_de_DotNetia.Models;
+
+internal class GardienEngourdi : Ennemi
+{
+  //prop
+
+  private bool _protectionIntacte = true;
+
+  private double ReductionDegats
+  {
+    get { return _protectionIntacte ? 0.25 : 1.0; } // si pas reçu de coup alors -75 % de degats
+  }
+
+  //constructeur
+
+  internal GardienEngourdi(string pseudo) : base(pseudo)
+  {
+  }
+
+  //override
+  protected override double MultiplicateurDegatsRecus => ReductionDegats;
+
+  protected override int BasePv => 550;
+
+
+  protected override void ApresReceptionDegats()
+  {
+    _protectionIntacte = false;
+  }
+}
