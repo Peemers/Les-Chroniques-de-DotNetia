fbdeeea01f34a2b68c5615d2a3caddc6420bc90e
Math Peeters <pmathieu.peeters@gmail.com>
2026-01-03 13:04:53 +0100

    ajout zone Desert.cs

---
+++ Les Chroniques de DotNetia/Models/Zones/Desert.cs
@@ -0,0 +1,14 @@
+﻿namespace Les_Chroniques_de_DotNetia.Models.Zones;
+using Les_Chroniques_de_DotNetia.Models.Ennemis;
+
+internal class Desert : Zone
+{
+  //constructeur
+
+  internal Desert() : base("Le Désert Sandboxé", "Un désert immense, chaud, sec, grouillant de monstres plus grands que la normale ")
+  {
+    EnnemisPossibles.Add(() => new SerpentGéant());
+    EnnemisPossibles.Add(() => new LezardVenimeux());
+    EnnemisPossibles.Add(() => new ScorpionGéant());
+  }
+}
