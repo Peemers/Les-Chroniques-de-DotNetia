0653533fa5a714792ec0144a7ff45c2b111fe945
Math Peeters <pmathieu.peeters@gmail.com>
2026-01-06 16:17:44 +0100

    menu de combat provisoir (essais des délégates dans program)

---
+++ Les Chroniques de DotNetia/Models/UI/MenuCombat.cs
@@ -0,0 +1,37 @@
+﻿using Les_Chroniques_de_DotNetia.Interfaces;
+using Les_Chroniques_de_DotNetia.Models;
+using Les_Chroniques_de_DotNetia.Models.Zones;
+
+namespace Les_Chroniques_de_DotNetia.Models.UI;
+
+internal delegate int ActionCombat(ICible cible);
+
+internal class MenuCombat
+{
+  private readonly Joueur _joueur;
+
+  internal MenuCombat(Joueur joueur)
+  {
+    _joueur = joueur;
+  }
+
+  internal ActionCombat ChoisirAction()
+  {
+    Console.WriteLine("Choisissez une action :");
+    Console.WriteLine("1 - Attaquer");
+    Console.WriteLine("2 - Attaque Lourde");
+
+    string choix = Console.ReadLine()!;
+
+    switch (choix)
+    {
+      case "1":
+        return _joueur.Attaquer;
+      case "2":
+        return _joueur.AttaqueLourde;
+      default:
+        Console.WriteLine("Choix invalide, attaque par défaut.");
+        return _joueur.Attaquer;
+    }
+  }
+}
--- Les Chroniques de DotNetia/Program.cs
+++ Les Chroniques de DotNetia/Program.cs
@@ -1,21 +1,26 @@
-﻿using Les_Chroniques_de_DotNetia.Interfaces;
-using Les_Chroniques_de_DotNetia.Models;
+﻿using Les_Chroniques_de_DotNetia.Models;
 using Les_Chroniques_de_DotNetia.Models.Ennemis;
-using Les_Chroniques_de_DotNetia.Models.Zones;
-
-internal delegate int FightAction(ICible cible);
+using Les_Chroniques_de_DotNetia.Models.UI;
 
 class Program
 {
   static void Main()
   {
-    FightAction action;
-    
     Joueur joueur1 = new Guerrier("Peemers");
     Ennemi ennemi1 = new Sanglier();
 
-    action = joueur1.Attaquer;
+    while (joueur1.IsAlive || ennemi1.IsAlive)
+    {
+      MenuCombat menuCombat = new MenuCombat(joueur1);
 
-    int degats = action(ennemi1);
+      ActionCombat actionChoisie = menuCombat.ChoisirAction();
+
+      int degats = actionChoisie(ennemi1);
+
+      Console.WriteLine($"Dégâts infligés : {degats}");
+      Console.WriteLine($"PV restants de l'ennemi : {ennemi1.PvActuels}");
+
+      Console.ReadKey();
+    }
   }
 }
